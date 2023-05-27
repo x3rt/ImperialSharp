6d911898017e12b86dcf323e8ed374bae4f83e63
x3rt <51767230+x3rt@users.noreply.github.com>
2023-05-27 06:20:14 -0600

    Support netstandard2.0

---
+++ src/ImperialSharp/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ImperialSharp.Extensions;
+
+internal static class HttpClientExtensions
+{
+    internal static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri,
+        HttpContent content)
+    {
+        var method = new HttpMethod("PATCH");
+        var request = new HttpRequestMessage(method, requestUri)
+        {
+            Content = content,
+        };
+        return await client.SendAsync(request);
+    }
+}
