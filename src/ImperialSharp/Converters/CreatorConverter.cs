82d30f989a3cdaa81351c4a3f59e8a3d8c9102d0
x3rt <51767230+x3rt@users.noreply.github.com>
2023-05-27 08:23:38 -0600

    Create a custom converter

---
+++ src/ImperialSharp/Converters/CreatorConverter.cs
@@ -0,0 +1,39 @@
+﻿using ImperialSharp.Types;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ImperialSharp.Converters;
+
+internal class CreatorConverter : JsonConverter<Creator>
+{
+    public override void WriteJson(JsonWriter writer, Creator? value, JsonSerializer serializer)
+    {
+        if (value == null)
+        {
+            writer.WriteNull();
+            return;
+        }
+
+        writer.WriteValue(value.Username);
+    }
+
+    public override Creator? ReadJson(JsonReader reader, Type objectType, Creator? existingValue, bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        var jObject = serializer.Deserialize<JObject>(reader);
+
+        if (jObject == null)
+            return null;
+
+        var id = jObject["id"]?.Value<string>();
+        var documentsMade = jObject["documents_made"]?.Value<int>();
+        var username = jObject["username"]?.Value<string>();
+        var flags = jObject["flags"]?.Value<int>();
+        var icon = jObject["icon"]?.Value<string>();
+
+        if (id == null || documentsMade == null || username == null || flags == null)
+            return null;
+
+        return new Creator(id, documentsMade.Value, username, flags.Value, icon);
+    }
+}
