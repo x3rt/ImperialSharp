465fdaee662dd7c4b518d0b604cd1956f62e4c28
x3rt <51767230+x3rt@users.noreply.github.com>
2023-05-27 08:24:31 -0600

    Create builders

--- src/ImperialSharp/Types/DocumentSettings.cs
+++ src/ImperialSharp/Types/DocumentSettings.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using ImperialSharp.Converters;
+using Newtonsoft.Json;
 
 namespace ImperialSharp.Types;
 
@@ -11,6 +12,106 @@ public class DocumentSettings
     [JsonProperty("password")] public string Password { private get; set; } = string.Empty;
     [JsonProperty("public")] public bool Public { get; set; }
     [JsonProperty("editors")] public Creator[] Editors { get; set; } = Array.Empty<Creator>();
+
+
+    /// <summary>
+    /// Sets the language of the document.
+    /// </summary>
+    /// <param name="language">The language to set.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithLanguage(string language)
+    {
+        Language = language;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets whether or not to embed images.
+    /// </summary>
+    /// <param name="imageEmbed">Whether or not to embed images.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithImageEmbed(bool imageEmbed = true)
+    {
+        ImageEmbed = imageEmbed;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets whether or not to enable instant delete.
+    /// </summary>
+    /// <param name="instantDelete">Whether or not to enable instant delete.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithInstantDelete(bool instantDelete = true)
+    {
+        InstantDelete = instantDelete;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets whether or not to encrypt the document.
+    /// </summary>
+    /// <param name="encrypted">Whether or not to encrypt the document.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithEncryption(bool encrypted = true)
+    {
+        Encrypted = encrypted;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the password of the document.
+    /// </summary>
+    /// <param name="password">The password to set.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithPassword(string password)
+    {
+        Password = password;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets whether or not the document is public.
+    /// </summary>
+    /// <param name="isPublic">Whether or not the document is public.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithPublic(bool isPublic = true)
+    {
+        Public = isPublic;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the editors of the document.
+    /// </summary>
+    /// <param name="editors">The editors to set.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithEditors(params Creator[] editors)
+    {
+        Editors = editors;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the editors of the document.
+    /// </summary>
+    /// <param name="editors">The editors to set.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithEditors(IEnumerable<Creator> editors)
+    {
+        Editors = editors.ToArray();
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the editors of the document.
+    /// </summary>
+    /// <param name="editors">The editors to set.</param>
+    /// <returns>The current instance of <see cref="DocumentSettings"/>.</returns>
+    public DocumentSettings WithEditors(params string[] editors)
+    {
+        Editors = editors.Select(x => new Creator(x)).ToArray();
+        return this;
+    }
 
     public override string ToString()
     {
